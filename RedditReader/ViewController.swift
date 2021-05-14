726938320dec2f4cd24a19e1e9cee4d678159c18
Juan Carlos González <juancarlos@ip-192-168-0-2.ec2.internal>
2021-05-13 20:06:14 -0500

    Initial commit

---
+++ RedditReader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RedditReader
+//
+//  Created by Juan Carlos on 13/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
